42568c41cc17e133bc5edc749c416d1372c89f2c
Tain Wang <Tianran1126>
2022-05-26 11:01:12 +0100

    Ability to display

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,14 @@ from django.urls import path
 from . import views
 
 app_name='quiz'
-urlpatterns=[path('',views.show_questions,name='vore')]
+urlpatterns=[path('',views.Question_View.as_view(),name='index')]
+
+
+"""
+app_name='polls'
+urlpatterns=[path('',views.IndexView.as_view(),name='index'),
+             path('specifics/<int:pk>/',views.DetailView.as_view(),name='detail'),
+             path('<int:pk>/results/',views.ResultsView.as_view(),name='results'),
+             path('<int:question_id>/vote/',views.vote,name='vote'),
+]
+"""
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.views import generic
 # Create your views here.
 
 def show_questions(request):
     return HttpResponse("Here all the questions")
+
+class Question_View(generic.ListView): 
+    context_object_name='questions'
+    template_name='polls/index.html'
+    def get_queryset(self):
+        return Question.objects.all()
